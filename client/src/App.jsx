aa15786e0cc37017bbbd49b6a882f4ae985b4b29
Prashant Tyagi <prashantpttyagi@gmail.com>
2024-02-19 17:24:00 +0530

    Created file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
-import react from "react";
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProfilePage from "./pages/ProfilePage";
 export default function App(){
-  return(<h1 className="text-red-600">App</h1 >);
+  return(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+    </Routes>
+  </BrowserRouter>
+  );
 }
