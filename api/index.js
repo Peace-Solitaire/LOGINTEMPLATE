e257eb9c6b15c36dc4a3775eec7e4ae821614149
Prashant Tyagi <prashantpttyagi@gmail.com>
2024-02-29 01:14:20 +0530

    __dirname error resolved

--- api/index.js
+++ api/index.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
-/* ------------------------------- Deploy code for Render -------------------------------*/
 const path = require("path");
-
-const __dirname = path.resolve();
-const app = express();
-
-app.use(express.static(path.join(__dirname, "client/dist")));
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
-/* ------------------------------- Deploy code for Render -------------------------------*/
-app.use(express.json());
-app.use(cookieParser());
-
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const connectDB = require("./config/db.js");
 connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 const userRoutes = require("./routes/userRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
